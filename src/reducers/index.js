aade9fd20e4d8c459c44e1608abefb57333cf20d
Minajeanne <albrown07@mac.com>
2019-09-12 15:17:56 -0400

    correction

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -3,15 +3,11 @@ import moveObjects from './moveObjects';
 import startGame from './startGame';
 
 const initialGameState = {
-  // ... other initial properties ...
-  flyingObjects: [],
-  lastObjectCreatedAt: new Date(),
-};
-
-const initialGameState = {
   started: false,
   kills: 0,
   lives: 3,
+  flyingObjects: [],
+  lastObjectCreatedAt: new Date(),
 };
 
 const initialState = {
