f7c2f4374b27fedc35d7462e49c633f161f5c9aa
Minajeanne <albrown07@mac.com>
2019-09-12 16:18:02 -0400

    correction

--- src/utils/formulas.js
+++ src/utils/formulas.js
@@ -30,7 +30,7 @@ export const getCanvasPosition = (event) => {
   // mouse position on auto-scaling canvas
   // https://stackoverflow.com/a/10298843/1232793
 
-  const svg = document.getElementById('aliens-go-home-canvas');
+  const svg = document.getElementById('test-game-canvas');
   const point = svg.createSVGPoint();
 
   point.x = event.clientX;
